3908a9046d69693c1a8f8a7ae041feb9b0a1fb04
pianomister <5443951+pianomister@users.noreply.github.com>
2023-12-28 21:16:04 +0100

    Fix display of start time minutes

--- src/Appointment.tsx
+++ src/Appointment.tsx
@@ -37,7 +37,7 @@ const Appointment: FC<Props> = ({ data, showRemoved = false }: Props) => {
   const date = new Date(data.date)
   const hours = date.getHours()
   let minutes: number | string = date.getMinutes()
-  minutes = minutes === 0 ? '00' : minutes
+  minutes = minutes < 10 ? `0${minutes}` : minutes
 
   const weekday = new Intl.DateTimeFormat(['en'], {
     weekday: 'short'
